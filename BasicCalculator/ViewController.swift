6edd2f58c0f58e07295a899bb82cc3bb93a16755
Burak Can Karasel <99825584+burakkarasel@users.noreply.github.com>
2023-08-03 12:01:29 +0300

    Basic Calculator v1 completed

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -8,12 +8,101 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstInput: UITextField!
+    @IBOutlet weak var secondInput: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func add(_ sender: Any) {
+        let resOfValidation = validateInputs(first: firstInput, second: secondInput)
+        if resOfValidation[0] == 0 && resOfValidation[1] == 0 {
+            result.text = "Only numbers are supported!"
+            result.textColor = UIColor.red
+            return
+        }
+        
+        result.text = "\(makeOperation(x: resOfValidation[0], y: resOfValidation[1], opt: "+"))"
+        result.textColor = UIColor.black
+    }
+    
+    @IBAction func subtract(_ sender: Any) {
+        let resOfValidation = validateInputs(first: firstInput, second: secondInput)
+        if resOfValidation[0] == 0 && resOfValidation[1] == 0 {
+            result.text = "Only numbers are supported!"
+            result.textColor = UIColor.red
+            return
+        }
+        
+        result.text = "\(makeOperation(x: resOfValidation[0], y: resOfValidation[1], opt: "-"))"
+        result.textColor = UIColor.black
+    }
+    
+    @IBAction func multiply(_ sender: Any) {
+        let resOfValidation = validateInputs(first: firstInput, second: secondInput)
+        if resOfValidation[0] == 0 && resOfValidation[1] == 0 {
+            result.text = "Only numbers are supported!"
+            result.textColor = UIColor.red
+            return
+        }
+        
+        result.text = "\(makeOperation(x: resOfValidation[0], y: resOfValidation[1], opt: "x"))"
+        result.textColor = UIColor.black
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+        let resOfValidation = validateInputs(first: firstInput, second: secondInput)
+        if resOfValidation[0] == 0 && resOfValidation[1] == 0 {
+            result.text = "Only numbers are supported!"
+            result.textColor = UIColor.red
+            return
+        }
+        if resOfValidation[1] == 0 {
+            result.text = "Cannot divide by 0!"
+            result.textColor = UIColor.red
+            return
+        }
+        
+        result.text = "\(makeOperation(x: resOfValidation[0], y: resOfValidation[1], opt: "/"))"
+        result.textColor = UIColor.black
+    }
+    
+    func makeOperation(x : Int, y : Int, opt: String) -> Int {
+        switch opt {
+        case "+":
+            return x + y
+        case "-":
+            return x - y
+        case "x":
+            return x * y
+        default:
+            return x / y
+        }
+    }
+    
+    func validateInputs(first: UITextField, second : UITextField) -> ([Int]) {
+        var firstOne = first.text
+        var secondOne = second.text
+        
+        if firstOne == nil {
+            return [0, 0]
+        }
+        
+        if secondOne == nil {
+            return [0, 0]
+        }
+        
+        if let myFirst = Int(firstOne!){
+            if let mySecond = Int(secondOne!) {
+                return [myFirst, mySecond]
+            }
+        }
+        
+        return [0, 0]
+    }
 }
 
